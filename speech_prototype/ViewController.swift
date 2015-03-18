664405f53819e3cc7f0b055fcc2159b99779bc8c
Benjamin Clanet <benjaminclanet@MacBook-Air.local>
2015-03-18 14:01:51 +0100

    Initial Commit

---
+++ speech_prototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  speech_prototype
+//
+//  Created by Benjamin Clanet on 3/18/15.
+//  Copyright (c) 2015 Benjamin Clanet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
